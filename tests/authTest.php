369ddeca7057ddc9981071c96a06ad0ec28a4b3f
Null Terminator <jules.malic@gmail.com>
2018-11-16 23:04:03 +1100

    sd

---
+++ tests/authTest.php
@@ -0,0 +1,18 @@
+			
+<?php
+
+require "vendor/autoload.php";
+						try {
+    $account = new \PSN\Auth('email@psn.com', 'password');
+} catch (\PSN\AuthException $e) {
+    header('Content-Type: application/json');
+    print $e->GetError();
+    exit;
+}
+
+$tokens = $account->GetTokens();
+
+$user = new \PSN\User($tokens);
+
+printf('<p>Hello, %s!</p>', $user->me()->profile->onlineId);
+?>
